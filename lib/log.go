7862140537c28d27d6e2790890f1de3f0bd2ba8c
Hironobu <hiro@hironobu.org>
2015-01-03 04:05:07 +0900

    ログレベルを変更

--- lib/log.go
+++ lib/log.go
@@ -14,15 +14,16 @@ func GetLogInstance() *logrus.Logger {
 	if instance == nil {
 
 		// テキストフォーマットで出力
-		//logrus.SetFormatter(&logrus.TextFormatter{})
-		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetFormatter(&logrus.TextFormatter{})
+		//logrus.SetFormatter(&logrus.JSONFormatter{})
 
 		// 標準エラー出力に出す
 		logrus.SetOutput(os.Stderr)
 
 		// ログレベルの設定
 		instance = logrus.New()
-		instance.Level = logrus.DebugLevel
+		//instance.Level = logrus.DebugLevel
+		instance.Level = logrus.InfoLevel
 		//instance.SetOutput(os.Stderr)
 	}
 	return instance
